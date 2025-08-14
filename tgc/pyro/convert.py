7e781b6a2499cccdecd72ff1810d859c13eafc2d
jumpau <jumpaulive@outlook.com>
2025-08-14 16:31:23 +0800

    Update convert.py

--- tgc/pyro/convert.py
+++ tgc/pyro/convert.py
@@ -1,103 +1,78 @@
+
 from hypy_utils.dict_utils import deep_dict
-from pyrogram.enums import MessageEntityType
-from pyrogram.parser import utils
-from pyrogram.types import MessageEntity, Message
-
+from telethon.tl.types import MessageEntityBold, MessageEntityItalic, MessageEntityCode, MessageEntityPre, MessageEntityTextUrl, MessageEntityUrl, MessageEntityMention, MessageEntityHashtag, MessageEntityCashtag, MessageEntityBotCommand, MessageEntityEmail, MessageEntityPhone, MessageEntityUnderline, MessageEntityStrike, MessageEntitySpoiler, Message
 from tgc.pyro.consts import MEDIA_TYPE_MAP
-
 
 def convert_media_dict(msg: Message) -> dict:
     def helper():
-        for f in ['photo', 'video', 'audio', 'voice', 'document', 'sticker', 'animation', 'video_note', 'contact',
-                  'location', 'venue', 'poll', 'web_page']:
-            dct = getattr(msg, f, None)
-            if dct:
-                return dict(vars(dct))
+        media = getattr(msg, 'media', None)
+        if media:
+            return dict(vars(media))
         return {}
-
     d = deep_dict(helper(), {'_client'})
     if d:
-        d['media_type'] = MEDIA_TYPE_MAP.get(msg.media)
-        if msg.has_media_spoiler:
-            d['spoiler'] = True
-
-    # Move location to one place
-    if msg.venue:
-        d.pop('location', None)
-        if msg.venue.location:
-            d['longitude'] = msg.venue.location.longitude
-            d['latitude'] = msg.venue.location.latitude
-
+        d['media_type'] = type(getattr(msg, 'media', None)).__name__
+        # Telethon 没有 has_media_spoiler，需自定义
+    # Telethon 没有 venue/location 结构，需自定义
     return d
 
 
-def entity_start_end(text: str, en: MessageEntity) -> tuple[str, str] | None:
+
+def entity_start_end(text: str, en: Message) -> tuple[str, str] | None:
     """
     Convert a message entity to a start tag and an end tag for HTML
     """
-    text = text[en.offset:en.offset + en.length]
-    match en.type:
-        case MessageEntityType.STRIKETHROUGH:
-            return "<del>", "</del>"
-        case MessageEntityType.CODE:
-            return "<code>", "</code>"
-        case MessageEntityType.ITALIC:
-            return "<em>", "</em>"
-        case MessageEntityType.UNDERLINE:
-            return "<u>", "</u>"
-        case MessageEntityType.BOLD:
-            return "<b>", "</b>"
-        case MessageEntityType.BLOCKQUOTE:
-            return "<blockquote>", "</blockquote>"
-        case MessageEntityType.SPOILER:
-            return '<span class="spoiler"><span>', '</span></span>'
-        case MessageEntityType.TEXT_LINK:
-            return f'<a href="{en.url}">', '</a>'
-        case MessageEntityType.URL:
-            return f'<a href="{text}">', '</a>'
-        case MessageEntityType.HASHTAG:
-            return f'<a href="#{text}">', '</a>'
-        case MessageEntityType.MENTION:
-            return f'<a href="https://t.me/{text.strip("@")}">', '</a>'
-        case MessageEntityType.CUSTOM_EMOJI:
-            # This will be replaced later
-            return f'<i class="custom-emoji" emoji-src="emoji/{en.custom_emoji_id}">', '</i>'
-        case MessageEntityType.PRE:
-            lang = en.language
-            return f'<pre language="{lang}">' if lang else f"<pre>", f"</pre>"
-        case _:
-            return None
+    # Telethon 的实体类型用 isinstance 判断
+    if isinstance(en, MessageEntityBold):
+        return ("<b>", "</b>")
+    if isinstance(en, MessageEntityItalic):
+        return ("<i>", "</i>")
+    if isinstance(en, MessageEntityCode):
+        return ("<code>", "</code>")
+    if isinstance(en, MessageEntityPre):
+        return ("<pre>", "</pre>")
+    if isinstance(en, MessageEntityTextUrl):
+        return (f'<a href="{en.url}">', "</a>")
+    if isinstance(en, MessageEntityUrl):
+        return ("<a>", "</a>")
+    if isinstance(en, MessageEntityMention):
+        return ("<span class='mention'>", "</span>")
+    if isinstance(en, MessageEntityHashtag):
+        return ("<span class='hashtag'>", "</span>")
+    if isinstance(en, MessageEntityCashtag):
+        return ("<span class='cashtag'>", "</span>")
+    if isinstance(en, MessageEntityBotCommand):
+        return ("<span class='botcommand'>", "</span>")
+    if isinstance(en, MessageEntityEmail):
+        return ("<span class='email'>", "</span>")
+    if isinstance(en, MessageEntityPhone):
+        return ("<span class='phone'>", "</span>")
+    if isinstance(en, MessageEntityUnderline):
+        return ("<u>", "</u>")
+    if isinstance(en, MessageEntityStrike):
+        return ("<s>", "</s>")
+    if isinstance(en, MessageEntitySpoiler):
+        return ("<span class='spoiler'>", "</span>")
+    return None
 
 
-def convert_text(text: str, entities: list[MessageEntity]) -> str:
+from typing import Any
+
+def convert_text(text: str, entities: list[Any]) -> str:
     """
     Convert text to HTML
     """
-    text = utils.add_surrogates(text)
-
+    # Pyrogram的surrogates工具可省略，直接处理字符串
     entities_offsets = []
-
     for entity in entities:
         start = entity.offset
         end = start + entity.length
-
         tags = entity_start_end(text, entity)
         if tags is None:
             continue
-
         entities_offsets.append((tags[0], start,))
         entities_offsets.append((tags[1], end,))
-
-    entities_offsets = map(
-        lambda x: x[1],
-        sorted(
-            enumerate(entities_offsets),
-            key=lambda x: (x[1][1], x[0]),
-            reverse=True
-        )
-    )
-
-    for entity, offset in entities_offsets:
-        text = text[:offset] + entity + text[offset:]
-
-    return utils.remove_surrogates(text)
+    entities_offsets = sorted(entities_offsets, key=lambda x: x[1], reverse=True)
+    for tag, offset in entities_offsets:
+        text = text[:offset] + tag + text[offset:]
+    return text

dc1ba9198141613e72340e551f9f5394ebec3f9a
jumpau <jumpaulive@outlook.com>
2025-08-14 16:30:15 +0800

    Update consts.py

--- tgc/pyro/consts.py
+++ tgc/pyro/consts.py
@@ -1,23 +1,19 @@
 from pathlib import Path
 
-from pyrogram.enums import MessageMediaType
+from telethon.tl.types import MessageMediaPhoto, MessageMediaDocument, MessageMediaContact, MessageMediaPoll, MessageMediaWebPage, MessageMediaGeo, MessageMediaVenue, MessageMediaAnimation, MessageMediaVideo, MessageMediaAudio, MessageMediaVoice
 
-MEDIA_TYPE_MAP: dict[MessageMediaType, str] = {
-    MessageMediaType.PHOTO: "photo",
-    MessageMediaType.STICKER: "sticker",
-    MessageMediaType.VOICE: "voice_message",
-    MessageMediaType.AUDIO: "audio_file",
-    MessageMediaType.ANIMATION: "animation",
-    MessageMediaType.VIDEO: "video_file",
-    MessageMediaType.VIDEO_NOTE: "video_file",
-    MessageMediaType.DOCUMENT: None,
-
-    # TODO: Support these in web ui
-    MessageMediaType.CONTACT: "contact",
-    MessageMediaType.POLL: "poll",
-    MessageMediaType.WEB_PAGE: "web_page",
-    MessageMediaType.LOCATION: "location",
-    MessageMediaType.VENUE: "location"
+MEDIA_TYPE_MAP = {
+    MessageMediaPhoto: "photo",
+    MessageMediaDocument: "document",
+    MessageMediaContact: "contact",
+    MessageMediaPoll: "poll",
+    MessageMediaWebPage: "web_page",
+    MessageMediaGeo: "location",
+    MessageMediaVenue: "location",
+    MessageMediaAnimation: "animation",
+    MessageMediaVideo: "video_file",
+    MessageMediaAudio: "audio_file",
+    MessageMediaVoice: "voice_message",
 }
 
 HTML = (Path(__file__).parent.parent / "tg-blog.html").read_text()
